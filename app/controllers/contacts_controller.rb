6040fff7f7594bd07b921c09812da11289315f8a
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-11-02 20:02:24 +0700

    Add validations of form on contacts page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,8 +3,15 @@ class ContactsController < ApplicationController
   end
 
   def create
+    
     @contact = Contact.new(contact_params)
-    @contact.save
+    
+    if @contact.valid?
+      @contact.save
+    else 
+      render action: 'new'
+    end
+
   end
 
   private
