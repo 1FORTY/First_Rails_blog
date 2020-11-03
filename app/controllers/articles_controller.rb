f8e91d7a431c1b2305db124d1943e4bcf804d788
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-11-04 00:20:51 +0700

    Add page about and terms and add templates for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Contact.new(article_params)
+    
+    if @article.valid?
+      @article.save
+    else 
+      render action: 'new'
+    end
+
     render plain: params[:article].inspect
   end
+
+    private
+    def contact_params
+      params.require(:article).permit(:email, :message)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'about/index'
+  get 'terms/index'
   get 'contacts/new'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
