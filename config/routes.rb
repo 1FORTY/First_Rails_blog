c942cc82e3ea631bf09e12b76ce58d11e905a524
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-11-08 22:33:30 +0700

    Add static pages pages#terms and pages#about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'about/index'
-  get 'terms/index'
   get 'contacts/new'
   get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
 
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
+
   resources :articles
 end
